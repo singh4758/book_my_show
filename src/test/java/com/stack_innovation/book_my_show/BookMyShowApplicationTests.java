d19bf2b7f50a87db2ed3b5f7f190c534ddf0505f
Abhishek Singh <singh4758@gmail.com>
2023-12-18 08:55:29 +0530

    intialize spring boot and add models folder

---
+++ src/test/java/com/stack_innovation/book_my_show/BookMyShowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stack_innovation.book_my_show;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookMyShowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
